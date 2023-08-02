07e2c70c1a164cd2007e185be47ca240ee99945b
Shubh Shahu <shubh14shahu@gmail.com>
2023-08-02 13:26:50 +0530

    Updates

---
+++ precache-manifest.695584869a727ba59a857deccc09e3b4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac33e8e014d729a2b929b45364925ecf",
+    "url": "/Taskmaster_v2/index.html"
+  },
+  {
+    "revision": "46c17109ab1fa695ed80",
+    "url": "/Taskmaster_v2/static/js/2.69b2e571.chunk.js"
+  },
+  {
+    "revision": "592483dbc0156d9869a8",
+    "url": "/Taskmaster_v2/static/js/main.75f74014.chunk.js"
+  },
+  {
+    "revision": "62db36bf639eaf96385e",
+    "url": "/Taskmaster_v2/static/js/runtime-main.1260a7ca.js"
+  }
+]);
